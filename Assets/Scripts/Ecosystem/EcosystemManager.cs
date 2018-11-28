98c9f9fe6fef51e2ed3d01a374423d0bf3cf075d
Michael Hodge <Michael.Hodge@iam.local>
2018-11-27 18:01:11 -0600

    Creates main scene, adds species base and ecosystem manager classes

---
+++ Assets/Scripts/Ecosystem/EcosystemManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcosystemManager : MonoBehaviour {
+
+    public float simulationTime = 5; // Minutes
+
+    // Species objects
+    public List<Species> humans = new List<Species>();
+    public List<Species> birds = new List<Species>();
+    public List<Species> fish = new List<Species>();
+    // Species amounts
+    private int maxHumans = 5;
+    private int maxBirds = 15;
+    private int maxFish = 25;
+    private int minSpecies = 2;
+
+	// Use this for initialization
+	void Start () {
+        // Create object pool for all needed species
+        PoolAllSpecies();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void PoolAllSpecies()
+    {
+        // Humans
+        for(int i = 0; i < maxHumans; i++)
+        {
+            GameObject aHuman = new GameObject();
+            aHuman.name = "A Human";
+            aHuman.AddComponent<Species>();
+            humans.Add(aHuman.GetComponent<Species>());
+        }
+    }
+}
---
+++ Assets/Scripts/Species/Species.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Species : MonoBehaviour {
+
+    protected string speciesName;   // Name of the species
+    protected float hunger;         // Hunger of the species
+    protected float health;         // Health of the species
+    protected bool breed;           // Can breed
+    protected bool stayMoving;      // Does the species need to stay moving?
+    protected bool isActive;        // Is the species active on the screen?
+    protected bool isAlive;         // Is the species dead?
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
